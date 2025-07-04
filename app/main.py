aa04e34e5a3a10a3c3052464d6f0f699a8318b99
61105926 <nicolas90gabo@gmail.com>
2025-07-04 00:25:15 -0400

    port 8001 localhost

--- app/main.py
+++ app/main.py
@@ -23,7 +23,4 @@ async def transcribe_audio(file: UploadFile = File(...)):
         "text": text
     }
 
-# Agregamos esto para correr directamente con python main.py si quieres probar local
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001)
+
